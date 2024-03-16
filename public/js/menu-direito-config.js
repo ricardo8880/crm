2e842d6b5990c101944fc2e43ffedb4565a1f7b0
ricardo8880 <ricardo-s-a@hotmail.com>
2024-03-16 17:59:40 -0300

    [FEAT] update dark

--- public/js/menu-direito-config.js
+++ public/js/menu-direito-config.js
@@ -1,29 +1,64 @@
     // Tema Dark
 
 
-    document.addEventListener('DOMContentLoaded', () => {
-        let tema_dark = document.querySelector("#tema-dark");
-        let menu_lateral = document.querySelector("#menu-lateral");
-        // Verificar e aplicar o tema dark baseado no valor armazenado
-        const temaDarkAtivo = localStorage.getItem('temaDark') === 'true';
-        
-        if (temaDarkAtivo) {
-            menu_lateral.classList.add('tema-dark');
-        }
+    // Tema Dark
+    // let tema_dark = document.querySelector("#tema-dark")
+    // tema_dark.addEventListener('click', ()=>{
+    //     let menu_lateral = document.querySelector('#menu-lateral')
+    //     menu_lateral.classList.toggle('tema-dark')
+    // })
 
-        tema_dark.addEventListener('click', () => {
-            // Alternar o estado do tema dark
-            const estaAtivo = menu_lateral.classList.contains('tema-dark');
-            if (estaAtivo) {
-                localStorage.setItem('temaDark', 'false');
-                menu_lateral.classList.remove('tema-dark');
-            } else {
-                localStorage.setItem('temaDark', 'true');
-                menu_lateral.classList.add('tema-dark');
+    class Tema {
+        constructor(temaDarkBtn, menuLateral) {
+            this.temaDarkBtn = temaDarkBtn;
+            this.menuLateral = menuLateral;
+    
+            // Verifica se o valor do localStorage está definido, caso contrário, define-o como 'false' para que o tema não seja escuro por padrão
+            if (localStorage.getItem('valor') === null) {
+                localStorage.setItem('valor', 'false');
             }
-        });
-
-    });
+    
+            // Adiciona o listener de evento ao botão do tema escuro
+            this.temaDarkBtn.addEventListener('click', this.toggleTema.bind(this));
+    
+            // Aplica o tema escuro ao carregar a página, se o valor do localStorage for 'true'
+            if (localStorage.getItem('valor') === 'true') {
+                this.aplicarTema();
+            }
+        }
+    
+        toggleTema() {
+            // Verifica se o tema atual é escuro ou claro
+            const temaAtual = localStorage.getItem('valor') === 'true';
+    
+            // Alterna entre os temas escuro e claro
+            if (temaAtual) {
+                this.desativarTema();
+            } else {
+                this.ativarTema();
+            }
+        }
+    
+        ativarTema() {
+            localStorage.setItem('valor', 'true'); // Define o valor como 'true' no localStorage
+            this.menuLateral.classList.add('tema-dark'); // Adiciona a classe 'tema-dark'
+        }
+    
+        desativarTema() {
+            localStorage.setItem('valor', 'false'); // Define o valor como 'false' no localStorage
+            this.menuLateral.classList.remove('tema-dark'); // Remove a classe 'tema-dark'
+        }
+    
+        aplicarTema() {
+            this.menuLateral.classList.add('tema-dark');
+        }
+    }
+    
+    // Instanciando a classe Tema com os elementos do DOM correspondentes
+    const tema = new Tema(
+        document.querySelector("#tema-dark"),
+        document.querySelector("#menu-lateral")
+    );
 
     // Menu lateral fixo
 
@@ -38,6 +73,9 @@
 
         menuFixo(menuFixoDados, colorMenu);
     });
+
+
+    
 
     // seletores
     document.addEventListener('DOMContentLoaded', function() {
@@ -60,4 +98,5 @@
     });
    
       
+   
    
